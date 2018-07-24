84aa99d677d9f13524db65146faec4948845492e
Niklas Trippler <Niklas.Trippler@knowit.no>
2018-07-24 07:19:11 +0000

    Added ServiceRouteMapper
    git-tfs-id: [http://tfs.knowitsolutions.tools/DefaultCollection]$/ApplikatorMain/Tools/ServiceMapper/ServiceMapper;C139392

---
+++ ServiceMapper/Mappers/ServiceRouteMapper.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceModel.Activation;
+
+namespace ServiceMapper.Mappers
+{
+	class ServiceRouteMapper : IMapper
+	{
+		private readonly INameMapper _nameMapper;
+		private readonly ServiceHostFactory _serviceHostFactory;
+
+		public ServiceRouteMapper(INameMapper nameMapper, ServiceHostFactory serviceHostFactory)
+		{
+			_nameMapper = nameMapper;
+			_serviceHostFactory = serviceHostFactory;
+		}
+		public object Map(Map type)
+		{
+			return new ServiceRoute(_nameMapper.Map(type), _serviceHostFactory, type.Type);
+		}
+	}
+}
